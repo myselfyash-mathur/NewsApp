f9907d647ac70b7419f66d873e6594a83f4ff0d6
Yash Mathur <yashmthr65@gmail.com>
2022-05-27 12:22:50 +0530

    Initial Changes

---
+++ src/components/News.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function News(){
+    return(
+        <div className='ui container'>
+            <h2> Displaying News!</h2>
+        </div>
+    );
+}
+
+export default News;
